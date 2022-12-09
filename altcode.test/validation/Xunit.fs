90b5dfcd1228a4b14d3bb6f418fabb980bc28a1d
Steve Gilham <SteveGilham@users.noreply.github.com>
2022-12-09 10:41:51 +0100

    And an Xunit case

--- altcode.test/validation/Xunit.fs
+++ altcode.test/validation/Xunit.fs
@@ -3,9 +3,23 @@ namespace AltCode.Validation
 module Xunit =
 
   open NUnit.Framework
-
-  [<SetUp>]
-  let Setup () = ()
+  open AltCode.Test.Xunit
 
   [<Test>]
-  let Test1 () = Assert.Pass()
+  let StringContainsShouldPass () =
+    let match1 =
+      { AssertionMatch.Create() with
+          Actual = "Hello"
+          Expected = "e" }
+
+    AltAssert.Contains match1
+
+  [<Test>]
+  let StringContainsShouldFail () =
+    let match1 =
+      { AssertionMatch.Create() with
+          Actual = "Hello"
+          Expected = "?" }
+
+    Assert.Throws<Xunit.Sdk.ContainsException>(fun _ -> AltAssert.Contains match1)
+    |> ignore
