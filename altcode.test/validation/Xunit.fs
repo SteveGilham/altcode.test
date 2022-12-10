04c9cc963b82c643a0bc2d5b09c361ed9bdb5ce2
Steve Gilham <SteveGilham@users.noreply.github.com>
2022-12-10 21:36:19 +0100

    Some Xunit -- 120 lines to go

--- altcode.test/validation/Xunit.fs
+++ altcode.test/validation/Xunit.fs
@@ -8,11 +8,19 @@ module Xunit =
   [<Test>]
   let StringContainsShouldPass () =
     let match1 =
-      { AssertionMatch.Create() with
-          Actual = "Hello"
-          Expected = "e" }
+      (AssertionMatch.Create().WithActual "Hello")
+        .WithExpected "e"
 
     AltAssert.Contains match1
+    AltAssert.Contains(match1, System.StringComparison.Ordinal)
+
+    let match2 =
+      (AssertionMatch.Create().WithActual "Hello")
+        .WithExpected "?"
+
+    AltAssert.DoesNotContain match2
+    AltAssert.DoesNotContain(match2, System.StringComparison.Ordinal)
+
 
   [<Test>]
   let StringContainsShouldFail () =
@@ -22,4 +30,21 @@ module Xunit =
           Expected = "?" }
 
     Assert.Throws<Xunit.Sdk.ContainsException>(fun _ -> AltAssert.Contains match1)
+    |> ignore
+
+    Assert.Throws<Xunit.Sdk.ContainsException>(fun _ ->
+      AltAssert.Contains(match1, System.StringComparison.Ordinal))
+    |> ignore
+
+    let match2 =
+      { AssertionMatch.Create() with
+          Actual = "Hello"
+          Expected = "e" }
+
+    Assert.Throws<Xunit.Sdk.DoesNotContainException>(fun _ ->
+      AltAssert.DoesNotContain match2)
+    |> ignore
+
+    Assert.Throws<Xunit.Sdk.DoesNotContainException>(fun _ ->
+      AltAssert.DoesNotContain(match2, System.StringComparison.Ordinal))
     |> ignore
