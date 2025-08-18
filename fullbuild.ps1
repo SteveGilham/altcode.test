224d081372b9c855f58cea7f730f13400850aad1
SteveGilham <SteveGilham@users.noreply.github.com>
2025-08-18 18:54:23 +0100

    Address review comments

--- fullbuild.ps1
+++ fullbuild.ps1
@@ -1,3 +1,3 @@
 dotnet tool restore
-dotnet run --project .\Build\setup.fsproj
-dotnet run --project .\Build\build.fsproj
+dotnet run --project .\Build\Setup.fsproj
+dotnet run --project .\Build\Build.fsproj
